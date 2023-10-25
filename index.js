72261fb5be88aaafc217df7c7c61db0de120d6ca
THOMAS RAMET <ramet.web@gmail.com>
2023-10-25 16:06:17 +0200

    OK

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,15 +18,36 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'helllo API'});
+  res.json({greeting: 'hello API'});
 });
 
+// Timestamp Microservice endpoint
+app.get("/api/:date?", (req, res) => {
+    let date;
 
+    // If no date parameter is provided, use the current date.
+    if (!req.params.date) {
+        date = new Date();
+    } else {
+        // Check if it's a Unix timestamp or a date string.
+        const isUnixTimestamp = /^\d+$/.test(req.params.date);
+        date = isUnixTimestamp ? new Date(parseInt(req.params.date)) : new Date(req.params.date);
+    }
+
+    // If the date is invalid, return an error.
+    if (isNaN(date.getTime())) {
+        res.json({ error: "Invalid Date" });
+    } else {
+        res.json({
+            unix: date.getTime(),
+            utc: date.toUTCString()
+        });
+    }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
